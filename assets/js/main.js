daf12a4014c8725eddca9e2981b74b0c77dfe580
mateus <mateushas.advir@gmail.com>
2024-01-08 13:31:18 -0300

    configurand cor por type de pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,14 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 function convertPokemonToLi(pokemon){
     return `
-    <li class="pokemon ${pokemon.type}">
+    <li class="pokemon ${pokemon.type[0]}">
                 <span class="number">#00${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
 
                     <div class="detail">
                         
                         <ol class="types">
-                           ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join(``) }
+                           ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join(``) }
                         </ol>
 
                         <img src="${pokemon.photo}" alt="${pokemon.name}">
@@ -21,6 +21,7 @@ function convertPokemonToLi(pokemon){
     </li>
         `
 }
+
  
 const pokemonList = document.getElementById(`pokemonList`);
 
